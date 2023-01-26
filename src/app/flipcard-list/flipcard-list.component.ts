a9ccd9016201e61fda1b471e6268161524cad06b
Luca-F <72333806+Luca-F@users.noreply.github.com>
2023-01-26 18:09:08 +0100

    3 files added

---
+++ src/app/flipcard-list/flipcard-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { flipcards } from '../flipcards';
+
+@Component({
+  selector: 'app-flipcard-list',
+  templateUrl: './flipcard-list.component.html',
+  styleUrls: ['./flipcard-list.component.css']
+})
+export class FlipcardListComponent implements OnInit {
+
+  flipcards = flipcards;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
