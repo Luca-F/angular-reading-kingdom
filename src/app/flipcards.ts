6eafb511cbdb17320913c807a206feb7df4407e4
Luca-F <72333806+Luca-F@users.noreply.github.com>
2023-01-26 18:04:06 +0100

    1 file added, 1 deleted

---
+++ src/app/flipcards.ts
@@ -0,0 +1,23 @@
+export interface Flipcard{
+  name: string;
+  description: string;
+  img: string;
+}
+
+export const flipcards =[
+  {
+    name:'Mira Lecocq',
+    description: 'Qui es tu ?',
+    img: '',
+  },
+  {
+    name:'Hugo Palazzi',
+    description: 'Qui es tu ?',
+    img: '',
+  },
+  {
+    name:'Luca Favetti',
+    description: 'Qui suis je?',
+    img: '',
+  },
+]
