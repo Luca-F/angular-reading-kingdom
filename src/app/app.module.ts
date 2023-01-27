b5afc84d4e58f2deb3a5a5e11e76d751ee745de8
Luca-F <72333806+Luca-F@users.noreply.github.com>
2023-01-27 16:00:59 +0100

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { BanComponent } from './ban/ban.component';
-import { ProposComponent } from './propos/propos.component'
+import { ProposComponent } from './propos/propos.component';
+import { LecturechapitreComponent  } from './lecturechapitre/lecturechapitre.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, BanComponent, ProposComponent ],
+  declarations: [ AppComponent, HelloComponent, BanComponent, ProposComponent, LecturechapitreComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/lecturechapitre/lecturechapitre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lecturechapitre',
+  templateUrl: './lecturechapitre.component.html',
+  styleUrls: ['./lecturechapitre.component.css']
+})
+export class LecturechapitreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
